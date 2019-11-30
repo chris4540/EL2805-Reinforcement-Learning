eeb2d12dda754e1d4afdee31f2d548c0666109c4
Chris Lin <chris4540@gmail.com>
2019-11-30 15:52:18 +0100

    Add draft of p2

--- lab1/problem_2.py
+++ lab1/problem_2.py
@@ -6,8 +6,10 @@ https://github.com/CarlosLing/KTH_Reinforcement_Learning/blob/master/Lab1/Proble
 https://github.com/dennybritz/reinforcement-learning/blob/master/DP/Value%20Iteration%20Solution.ipynb
 
 https://github.com/bsridatta/ReinforcementLearning/blob/master/maze.py
+
+https://github.com/OleguerCanal/KTH_RL-EL2805
 """
-from enum import Enum
+from enum import IntEnum
 import numpy as np
 
 
@@ -36,13 +38,15 @@ class Town:
     # The location of the police station
     POLICE_STATION = (1, 2)
 
-    #
+    # The rob init place
     ROB_INIT = BANKS[0]
 
 
 class State:
-    """
-    Abstractized the state S: S_p \times S_r
+    r"""
+    Abstractized the state S:
+    .. math::
+        S: S_p \times S_r
     """
 
     def __init__(self, police_state=None, rob_state=None):
@@ -89,39 +93,128 @@ class State:
     @staticmethod
     def size():
         """
-        As this
+        Return the size of State
         """
         ret = Town.SIZE ** 2
         return ret
 
+    def at_the_same_pos(self):
+        ret = self.police_state == self.rob_state
+        return ret
 
-class Action(Enum):
-    STAND = 'S'
-    UP = 'U'
-    DOWN = 'D'
-    LEFT = 'L'
-    RIGHT = 'R'
+    def __repr__(self):
+        ret = "Police: {}; Rob: {}".format(self.police_state, self.rob_state)
+        return ret
+
+
+class Action(IntEnum):
+    STAND = 0
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 
 def prepare_trans():
     state_size = State.size()
+    # trans.shape = [# of S', # of S, # of A]
     trans = np.zeros((state_size, state_size, len(Action)))
 
     # Loop over the all possible states
+    for state_idx in range(state_size):
+        s = State.from_idx(state_idx)
+        # consider a situaion when police and rob in the same grid
+        if s.at_the_same_pos:
+            s_pi = State()  # go back to inital cond
+            trans[s_pi.to_idx(), s.to_idx(), :] = 1.0
+            continue    # skip loop over actions
+
+        for a in Action:
+            trans = get_trans_prob(s, a)
 
     return trans
+
+
+def next_police_actions(state):
+    # unpack the state first
+    police_state, rob_state = state.unpack
+
+    # consider the random action of the police
+    police_row, police_col = police_state
+    rob_row, rob_col = rob_state
+
+    actions = list()
+
+    if police_row == rob_row and police_col == rob_col:
+        raise ValueError("Go to inital state")
+
+    # vertical movement
+    if police_row > rob_row:  # police is on rob's south
+        actions.append(Action.UP)
+    elif police_row < rob_row:
+        actions.append(Action.DOWN)
+    else:  # same row
+        actions.extend([Action.UP, Action.DOWN])
+
+    # horizontal movement
+    if police_col > rob_col:
+        actions.append(Action.LEFT)
+    elif police_col < rob_col:
+        actions.append(Action.RIGHT)
+    else:
+        actions.extend([Action.LEFT, Action.RIGHT])
+    return actions
 
 
 def get_trans_prob(state, action):
     """
     Return a dictionary of state and probability as a represtation of
         p(s' | s, a)
+    Args:
+        state ([type]): [description]
+        action ([type]): [description]
+
+    Returns:
+        dict:  dictionary of probabilities as a function of next state s'
     """
-    pass
+    # check the actions the police will take
+    next_police_actions = next_police_actions(state)
+
+
+def next_state_of_a_role(state, action):
+    """[summary]
+
+    Args:
+        state (tuple): the current state of the player (either police or rob)
+        action (enum of Action): [description]
+
+    Raises:
+        ValueError: if action is not valid
+
+    Returns:
+        tuple: a new state according to the action
+    """
+    row, col = state
+    # --------------------------------------------
+    # Consider the next state given the action
+    # --------------------------------------------
+    if action == Action.UP:
+        next_state = (max(row - 1, 0), col)
+    elif action == Action.LEFT:
+        next_state = (row, max(col - 1, 0))
+    elif action == Action.RIGHT:
+        next_state = (row, min(col + 1, Town.WIDTH - 1))
+    elif action == Action.DOWN:
+        next_state = (min(row + 1, Town.HEIGHT - 1), col)
+    elif action == Action.STAND:
+        next_state = (row, col)
+    else:
+        raise ValueError("Action for stepping is not valid")
+    # --------------------------------------------
+    return next_state
 
 
 def value_iter_once(v_fun):
-
     return v_fun
 
 
